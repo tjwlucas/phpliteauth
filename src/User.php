011fd3fe9d0cf6db62c5af6ac20f47bb21522dda
Thomas Lucas <tom@thomaslucas.co.uk>
2017-04-27 16:11:00 +0100

    Specify columns to fetch, and put all on user object

--- src/User.php
+++ src/User.php
@@ -7,16 +7,15 @@ class User
     public function __construct($auth, $id)
     {
         $this->id = $id;
-        $info = $auth->db->get('liteauth_users', '*', ['id' => $id]);
-        $this->username = $info['user'];
-        $this->admin = $info['admin'];
+        $info = $auth->db->get('liteauth_users', ['user', 'first_name', 'surname', 'email', 'admin'], ['id' => $id]);
         if(isset($info['first_name']) && isset($info['surname']))
             $this->name = $info['first_name'] . ' ' . $info['surname'];
         elseif(isset($info['first_name']))
             $this->name = $info['first_name'];
         else
             $this->name = $this->username;
-        $this->email = $info['email'];
+        foreach($info as $key => $value)
+            $this->$key = $value;
         return $this;
     }
 }
