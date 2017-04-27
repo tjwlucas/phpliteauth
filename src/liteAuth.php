6713c106f51cdb962b7da8ba7b2d2bad66c0b66f
Thomas Lucas <tom@thomaslucas.co.uk>
2017-04-27 09:35:25 +0100

    Added basic user creation and password verification

--- src/liteAuth.php
+++ src/liteAuth.php
@@ -21,13 +21,28 @@ class liteAuth
     }
 
     private function runmigrations() {
-        $next = $this->db->get('liteauth_migrations', 'id', ['run'=>1, "ORDER" => ['id' => 'DESC']]) + 1;
+        $next = $this->db->get('liteauth_migrations', 'id', [ "ORDER" => ['id' => 'DESC']]) + 1;
         while(file_exists(__DIR__.'/db/'.$next.'.sql'))
         {
             $sql = file_get_contents(__DIR__.'/db/'.$next.'.sql');
             $this->db->query($sql); 
-            $this->db->insert('liteauth_migrations', ['id' => $next, 'run' => 1]);
+            $this->db->query("insert into liteauth_migrations (id, run) values ( $next , CURRENT_TIMESTAMP );");
             $next++;
         }
+    }
+
+    public function newUser($user, $pass){
+        $hash = password_hash($pass, PASSWORD_BCRYPT);
+        if($this->db->get('liteauth', ['user' => $user]))
+            throw new \Exception('User already exists');
+        $this->db->insert('liteauth', ['user' => $user, 'pass' => $hash]);
+    }
+
+    public function authUser($user, $pass){
+        $hash = password_hash($pass, PASSWORD_BCRYPT);
+        $record = $this->db->get('liteauth', ['user', 'pass'], ['user' => $user]);
+        if(!$record)
+            throw new \Exception('User does not exist');
+        return password_verify($pass, $record['pass']);
     }
 }
