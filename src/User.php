940cbbc9ee11de40b61690edbb5ff596404bcf36
Thomas Lucas <tom@thomaslucas.co.uk>
2017-04-27 20:40:00 +0100

    Added save method to user to allow updating user properties in database

--- src/User.php
+++ src/User.php
@@ -4,18 +4,33 @@ namespace liteAuth;
 
 class User
 {
+    private $auth;
     public function __construct($auth, $id)
     {
         $this->id = $id;
+        $this->auth = $auth;
         $info = $auth->db->get('liteauth_users', ['user', 'first_name', 'surname', 'email', 'admin'], ['id' => $id]);
         foreach($info as $key => $value)
             $this->$key = $value;
-        if(isset($info['first_name']) && isset($info['surname']))
-            $this->name = $info['first_name'] . ' ' . $info['surname'];
-        elseif(isset($info['first_name']))
-            $this->name = $info['first_name'];
-        else
-            $this->name = $this->user;
         return $this;
+    }
+
+    public function name()
+    {
+        if(isset($this->first_name) && isset($this->surname))
+            return $this->first_name . ' ' . $this->surname;
+        elseif(isset($this->first_name))
+            return $this->first_name;
+        else
+            return $this->user;
+    }
+
+    public function save()
+    {
+        $reflection = new \ReflectionObject($this);
+        $properties = $reflection->getProperties(\ReflectionProperty::IS_PUBLIC);
+        foreach($properties as $prop)
+            $info[$prop->name] = $this->{$prop->name};
+        return $this->auth->db->update('liteauth_users', $info, ['id' => $this->id]);
     }
 }
